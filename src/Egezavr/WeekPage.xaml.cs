e6a1bb19746d805f437e8cbf9c2e33b74877107f
te9c <roman.te9c@gmail.com>
2023-03-22 16:31:15 +0500

    rev 0.2.3

--- src/Egezavr/WeekPage.xaml.cs
+++ src/Egezavr/WeekPage.xaml.cs
@@ -2,14 +2,20 @@ namespace Egezavr;
 
 public partial class WeekPage : ContentPage
 {
-
 	public WeekPage()
 	{
 		InitializeComponent();
+
 	}
 
-	private void MondayAddButtonClicked(object sender,EventArgs e)
+	private async void AddButtonClicked(object sender,EventArgs e)
 	{
+		Button button = sender as Button ?? throw new ArgumentException($"{sender} is not Button type");
+		
+		Border buttonBorder = button.Parent as Border ?? throw new ArgumentException($"Parent of {button} is not Border");
 
-	}
+        VerticalStackLayout dayStack = buttonBorder.Parent as VerticalStackLayout ?? throw new ArgumentException($"Parent of {buttonBorder} is not VerticalStackLayout");
+
+        var action = await DisplayActionSheet("Выбрать язык", "Отмена", "Удалить", "C#", "JavaScript", "Java");
+    }
 }
